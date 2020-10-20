d75469b0cf10de52fcc28f3966a0125b8fa83ac0
Mateusz Czajka <czajkamateusz999@gmail.com>
2020-10-20 13:16:50 +0200

    enable sending emails (test, prod), plain/HTML emails confirmation, slow-running code Active Job, tests

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,16 @@
 Rails.application.configure do
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  config.action_mailer.smtp_settings = {
+    address:          'smtp.gmail.com',
+    port:             587,
+    authentication:   'plain',
+    user_name:        'dave',
+    password:         'secret',
+    enable_starttls_auto: true
+  }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -1,6 +1,8 @@
 require "application_system_test_case"
 
 class OrdersTest < ApplicationSystemTestCase
+  include ActiveJob::TestHelper
+
   test "visiting the index" do
     visit orders_url
     assert_selector "h1", text: "Orders"
@@ -16,6 +18,9 @@ class OrdersTest < ApplicationSystemTestCase
   end
 
   test 'check routing number' do
+    LineItem.delete_all
+    Order.delete_all
+
     visit store_index_url
 
     click_on 'Add to Cart', match: :first
@@ -31,5 +36,28 @@ class OrdersTest < ApplicationSystemTestCase
     select 'Check', from: 'pay_type'
 
     assert_selector '#order_routing_number'
+
+    fill_in "Routing #", with: '123456'
+    fill_in "Account #", with: '987654'
+
+    perform_enqueued_jobs do
+      click_button "Update Order"
+    end
+
+    orders = Order.all
+    assert_equal 1, orders.size
+
+    order = orders.first
+
+    assert_equal 'Dave Thomas', order.name
+    assert_equal '123 Main Street', order.address
+    assert_equal 'dave@example.com', order.email
+    assert_equal 'Check', order.pay_type
+    assert_equal 1, order.line_items.size
+    
+    mail = ActionMailer::Base.deliveries.last
+    assert_equal ['dave@example.com'], mail.to
+    assert_equal 'Sam Ruby <depot@example.com>', mail[:from].value
+    assert_equal 'Pragmatic Store Order Confirmation', mail.subject
   end
 end
